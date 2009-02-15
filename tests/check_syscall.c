50cd075f0cc00c3e7f2617c4975a25273cb5baca
Ali Polatel <polatel@gmail.com>
2009-02-15 18:43:34 +0000

    More tests.

--- tests/check_syscall.c
+++ tests/check_syscall.c
@@ -289,6 +289,277 @@ START_TEST(check_syscall_check_chmod_predict_write_allow) {
 }
 END_TEST
 
+START_TEST(check_syscall_check_chown_deny) {
+    pid_t pid;
+
+    pid = fork();
+    if (0 > pid)
+        fail("fork() failed: %s", strerror(errno));
+    else if (0 == pid) { /* child */
+        ptrace(PTRACE_TRACEME, 0, NULL, NULL);
+        kill(getpid(), SIGSTOP);
+        chown("/dev/null", 0, 0);
+    }
+    else { /* parent */
+        int status, syscall;
+        context_t *ctx = context_new();
+        struct decision decs;
+
+        tchild_new(&(ctx->children), pid);
+        ctx->eldest = ctx->children;
+        wait(&status);
+        fail_unless(WIFSTOPPED(status),
+                "child %i didn't stop by sending itself SIGSTOP",
+                pid);
+        tchild_setup(ctx->eldest);
+
+        /* Resume the child, it will stop at the next system call. */
+        fail_unless(0 == ptrace(PTRACE_SYSCALL, pid, NULL, NULL),
+                "PTRACE_SYSCALL failed: %s", strerror(errno));
+        wait(&status);
+        fail_unless(WIFSTOPPED(status),
+                "child %i didn't stop by sending itself SIGTRAP",
+                pid);
+
+        syscall = ptrace_get_syscall(pid);
+        decs = syscall_check(ctx, ctx->eldest, syscall);
+        fail_unless(R_DENY_VIOLATION == decs.res,
+                "Expected R_DENY_VIOLATION, got %d", decs.res);
+
+        kill(pid, SIGTERM);
+    }
+
+}
+END_TEST
+
+START_TEST(check_syscall_check_chown_predict) {
+    pid_t pid;
+
+    pid = fork();
+    if (0 > pid)
+        fail("fork() failed: %s", strerror(errno));
+    else if (0 == pid) { /* child */
+        ptrace(PTRACE_TRACEME, 0, NULL, NULL);
+        kill(getpid(), SIGSTOP);
+        chown("/dev/null", 0, 0);
+    }
+    else { /* parent */
+        int status, syscall;
+        context_t *ctx = context_new();
+        struct decision decs;
+
+        pathlist_init(&(ctx->predict_prefixes), "/home/emily:/dev:/tmp");
+        tchild_new(&(ctx->children), pid);
+        ctx->eldest = ctx->children;
+
+        wait(&status);
+        fail_unless(WIFSTOPPED(status),
+                "child %i didn't stop by sending itself SIGSTOP",
+                pid);
+        tchild_setup(ctx->eldest);
+
+        /* Resume the child, it will stop at the next system call. */
+        fail_unless(0 == ptrace(PTRACE_SYSCALL, pid, NULL, NULL),
+                "PTRACE_SYSCALL failed: %s", strerror(errno));
+        wait(&status);
+        fail_unless(WIFSTOPPED(status),
+                "child %i didn't stop by sending itself SIGTRAP",
+                pid);
+
+        syscall = ptrace_get_syscall(pid);
+        decs = syscall_check(ctx, ctx->eldest, syscall);
+        fail_unless(R_DENY_RETURN == decs.res,
+                "Expected R_DENY_RETURN, got %d", decs.res);
+        fail_unless(0 == decs.ret,
+                "Expected 0 got %d", decs.ret);
+
+        kill(pid, SIGTERM);
+    }
+}
+END_TEST
+
+START_TEST(check_syscall_check_chown_allow) {
+    pid_t pid;
+
+    pid = fork();
+    if (0 > pid)
+        fail("fork() failed: %s", strerror(errno));
+    else if (0 == pid) { /* child */
+        ptrace(PTRACE_TRACEME, 0, NULL, NULL);
+        kill(getpid(), SIGSTOP);
+        chown("/dev/null", 0, 0);
+    }
+    else { /* parent */
+        int status, syscall;
+        context_t *ctx = context_new();
+        struct decision decs;
+
+        pathlist_init(&(ctx->write_prefixes), "/home/emily:/dev:/tmp");
+        tchild_new(&(ctx->children), pid);
+        ctx->eldest = ctx->children;
+
+        wait(&status);
+        fail_unless(WIFSTOPPED(status),
+                "child %i didn't stop by sending itself SIGSTOP",
+                pid);
+        tchild_setup(ctx->eldest);
+
+        /* Resume the child, it will stop at the next system call. */
+        fail_unless(0 == ptrace(PTRACE_SYSCALL, pid, NULL, NULL),
+                "PTRACE_SYSCALL failed: %s", strerror(errno));
+        wait(&status);
+        fail_unless(WIFSTOPPED(status),
+                "child %i didn't stop by sending itself SIGTRAP",
+                pid);
+
+        syscall = ptrace_get_syscall(pid);
+        decs = syscall_check(ctx, ctx->eldest, syscall);
+        fail_unless(R_ALLOW== decs.res,
+                "Expected R_ALLOW, got %d", decs.res);
+
+        kill(pid, SIGTERM);
+    }
+}
+END_TEST
+
+START_TEST(check_syscall_check_chown_predict_write_deny) {
+    pid_t pid;
+
+    pid = fork();
+    if (0 > pid)
+        fail("fork() failed: %s", strerror(errno));
+    else if (0 == pid) { /* child */
+        ptrace(PTRACE_TRACEME, 0, NULL, NULL);
+        kill(getpid(), SIGSTOP);
+        chown("/dev/null", 0, 0);
+    }
+    else { /* parent */
+        int status, syscall;
+        context_t *ctx = context_new();
+        struct decision decs;
+
+        pathlist_init(&(ctx->predict_prefixes), "/home/see:/usr/emily:/tmp/play");
+        pathlist_init(&(ctx->write_prefixes), "/home/arnold:/dev/layne");
+        tchild_new(&(ctx->children), pid);
+        ctx->eldest = ctx->children;
+
+        wait(&status);
+        fail_unless(WIFSTOPPED(status),
+                "child %i didn't stop by sending itself SIGSTOP",
+                pid);
+        tchild_setup(ctx->eldest);
+
+        /* Resume the child, it will stop at the next system call. */
+        fail_unless(0 == ptrace(PTRACE_SYSCALL, pid, NULL, NULL),
+                "PTRACE_SYSCALL failed: %s", strerror(errno));
+        wait(&status);
+        fail_unless(WIFSTOPPED(status),
+                "child %i didn't stop by sending itself SIGTRAP",
+                pid);
+
+        syscall = ptrace_get_syscall(pid);
+        decs = syscall_check(ctx, ctx->eldest, syscall);
+        fail_unless(R_DENY_VIOLATION == decs.res,
+                "Expected R_DENY_VIOLATION, got %d", decs.res);
+
+        kill(pid, SIGTERM);
+    }
+
+}
+END_TEST
+
+START_TEST(check_syscall_check_chown_predict_write_predict) {
+    pid_t pid;
+
+    pid = fork();
+    if (0 > pid)
+        fail("fork() failed: %s", strerror(errno));
+    else if (0 == pid) { /* child */
+        ptrace(PTRACE_TRACEME, 0, NULL, NULL);
+        kill(getpid(), SIGSTOP);
+        chown("/dev/null", 0, 0);
+    }
+    else { /* parent */
+        int status, syscall;
+        context_t *ctx = context_new();
+        struct decision decs;
+
+        pathlist_init(&(ctx->predict_prefixes), "/home/emily:/dev:/tmp");
+        pathlist_init(&(ctx->write_prefixes), "/home/arnold:/dev/layne");
+        tchild_new(&(ctx->children), pid);
+        ctx->eldest = ctx->children;
+
+        wait(&status);
+        fail_unless(WIFSTOPPED(status),
+                "child %i didn't stop by sending itself SIGSTOP",
+                pid);
+        tchild_setup(ctx->eldest);
+
+        /* Resume the child, it will stop at the next system call. */
+        fail_unless(0 == ptrace(PTRACE_SYSCALL, pid, NULL, NULL),
+                "PTRACE_SYSCALL failed: %s", strerror(errno));
+        wait(&status);
+        fail_unless(WIFSTOPPED(status),
+                "child %i didn't stop by sending itself SIGTRAP",
+                pid);
+
+        syscall = ptrace_get_syscall(pid);
+        decs = syscall_check(ctx, ctx->eldest, syscall);
+        fail_unless(R_DENY_RETURN == decs.res,
+                "Expected R_DENY_RETURN, got %d", decs.res);
+        fail_unless(0 == decs.ret,
+                "Expected 0 got %d", decs.ret);
+
+        kill(pid, SIGTERM);
+    }
+}
+END_TEST
+
+START_TEST(check_syscall_check_chown_predict_write_allow) {
+    pid_t pid;
+
+    pid = fork();
+    if (0 > pid)
+        fail("fork() failed: %s", strerror(errno));
+    else if (0 == pid) { /* child */
+        ptrace(PTRACE_TRACEME, 0, NULL, NULL);
+        kill(getpid(), SIGSTOP);
+        chown("/dev/null", 0, 0);
+    }
+    else { /* parent */
+        int status, syscall;
+        context_t *ctx = context_new();
+        struct decision decs;
+
+        pathlist_init(&(ctx->predict_prefixes), "/home/see:/usr/emily:/tmp/play");
+        pathlist_init(&(ctx->write_prefixes), "/var/tmp:/dev");
+        tchild_new(&(ctx->children), pid);
+        ctx->eldest = ctx->children;
+
+        wait(&status);
+        fail_unless(WIFSTOPPED(status),
+                "child %i didn't stop by sending itself SIGSTOP",
+                pid);
+        tchild_setup(ctx->eldest);
+
+        /* Resume the child, it will stop at the next system call. */
+        fail_unless(0 == ptrace(PTRACE_SYSCALL, pid, NULL, NULL),
+                "PTRACE_SYSCALL failed: %s", strerror(errno));
+        wait(&status);
+        fail_unless(WIFSTOPPED(status),
+                "child %i didn't stop by sending itself SIGTRAP",
+                pid);
+
+        syscall = ptrace_get_syscall(pid);
+        decs = syscall_check(ctx, ctx->eldest, syscall);
+        fail_unless(R_ALLOW == decs.res,
+                "Expected R_ALLOW, got %d", decs.res);
+
+        kill(pid, SIGTERM);
+    }
+}
+END_TEST
+
 Suite *syscall_suite_create(void) {
     Suite *s = suite_create("syscall");
 
@@ -300,6 +571,12 @@ Suite *syscall_suite_create(void) {
     tcase_add_test(tc_syscall_check, check_syscall_check_chmod_predict_write_deny);
     tcase_add_test(tc_syscall_check, check_syscall_check_chmod_predict_write_predict);
     tcase_add_test(tc_syscall_check, check_syscall_check_chmod_predict_write_allow);
+    tcase_add_test(tc_syscall_check, check_syscall_check_chown_deny);
+    tcase_add_test(tc_syscall_check, check_syscall_check_chown_predict);
+    tcase_add_test(tc_syscall_check, check_syscall_check_chown_allow);
+    tcase_add_test(tc_syscall_check, check_syscall_check_chown_predict_write_deny);
+    tcase_add_test(tc_syscall_check, check_syscall_check_chown_predict_write_predict);
+    tcase_add_test(tc_syscall_check, check_syscall_check_chown_predict_write_allow);
     suite_add_tcase(s, tc_syscall_check);
 
     return s;
