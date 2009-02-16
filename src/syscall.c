ba96e36beaf59eab0d8ff4268ac3f3e0687cdd01
Ali Polatel <polatel@gmail.com>
2009-02-16 20:05:12 +0000

    Add CHECK_PATH2, use DONT_RESOLV instead of RESOLV_PATH, add mount,umount,umount2 and utime.

--- src/syscall.c
+++ src/syscall.c
@@ -19,6 +19,9 @@
  * Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+#define _ATFILE_SOURCE /* AT_FDCWD */
+
+#include <assert.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <limits.h>
@@ -37,8 +40,9 @@
 #define OPEN_MODE       (1 << 1) /* Check the mode argument of open() */
 #define ACCESS_MODE     (1 << 2) /* Check the mode argument of access() */
 #define CHECK_PATH      (1 << 3) /* First argument should be a valid path */
-#define RESOLV_PATH     (1 << 4) /* Resolve first pathname and modify syscall */
-#define NET_CALL        (1 << 5) /* Allowing the system call depends on the net flag */
+#define CHECK_PATH2     (1 << 4) /* Second argument should be a valid path */
+#define DONT_RESOLV     (1 << 5) /* Don't resolve symlinks */
+#define NET_CALL        (1 << 6) /* Allowing the system call depends on the net flag */
 
 /* System call dispatch table */
 static struct syscall_def {
@@ -46,19 +50,34 @@ static struct syscall_def {
     const char *name;
     unsigned int flags;
 } system_calls[] = {
-    {__NR_chmod,        "chmod",        CHECK_PATH | RESOLV_PATH},
-    {__NR_chown,        "chown",        CHECK_PATH | RESOLV_PATH},
-    {__NR_open,         "open",         CHECK_PATH | RESOLV_PATH | RETURNS_FD | OPEN_MODE},
-    {__NR_creat,        "creat",        CHECK_PATH | RESOLV_PATH},
-    {__NR_lchown,       "lchown",       CHECK_PATH},
-    {__NR_link,         "link",         CHECK_PATH | RESOLV_PATH},
-    {__NR_mkdir,        "mkdir",        CHECK_PATH | RESOLV_PATH},
-    {__NR_mknod,        "mknod",        CHECK_PATH | RESOLV_PATH},
-    {__NR_access,       "access",       CHECK_PATH | RESOLV_PATH | ACCESS_MODE},
-    {__NR_rename,       "rename",       CHECK_PATH | RESOLV_PATH},
-    {__NR_rmdir,        "rmdir",        CHECK_PATH | RESOLV_PATH},
-    {__NR_symlink,      "symlink",      CHECK_PATH | RESOLV_PATH},
-    {__NR_truncate,     "truncate",     CHECK_PATH | RESOLV_PATH},
+    {__NR_chmod,        "chmod",        CHECK_PATH},
+    {__NR_chown,        "chown",        CHECK_PATH},
+#if defined(I386)
+    {__NR_chown32,      "chown32",      CHECK_PATH},
+#endif
+    {__NR_open,         "open",         CHECK_PATH | RETURNS_FD | OPEN_MODE},
+    {__NR_creat,        "creat",        CHECK_PATH},
+    {__NR_lchown,       "lchown",       CHECK_PATH | DONT_RESOLV},
+#if defined(I386)
+    {__NR_lchown32,     "lchown32",     CHECK_PATH | DONT_RESOLV},
+#endif
+    {__NR_link,         "link",         CHECK_PATH},
+    {__NR_mkdir,        "mkdir",        CHECK_PATH},
+    {__NR_mknod,        "mknod",        CHECK_PATH},
+    {__NR_access,       "access",       CHECK_PATH | ACCESS_MODE},
+    {__NR_rename,       "rename",       CHECK_PATH | CHECK_PATH2},
+    {__NR_rmdir,        "rmdir",        CHECK_PATH},
+    {__NR_symlink,      "symlink",      CHECK_PATH2 | DONT_RESOLV},
+    {__NR_truncate,     "truncate",     CHECK_PATH},
+#if defined(I386)
+    {__NR_truncate64,   "truncate64",   CHECK_PATH},
+#endif
+    {__NR_mount,        "mount",        CHECK_PATH},
+#if defined(I386)
+    {__NR_umount,       "umount",       CHECK_PATH},
+#endif
+    {__NR_umount2,      "umount2",      CHECK_PATH},
+    {__NR_utime,        "utime",        CHECK_PATH},
     {__NR_unlink,       "unlink",       CHECK_PATH},
     {__NR_openat,       "openat",       0},
     {__NR_mkdirat,      "mkdirat",      0},
@@ -78,6 +97,143 @@ static struct syscall_def {
     {0,                 NULL,           0}
 };
 
+int syscall_check_path(context_t *ctx, struct tchild *child,
+        struct decision *decs, int arg, int sflags, const char *sname) {
+    int issymlink;
+    char pathname[PATH_MAX];
+    char *rpath;
+
+    assert(1 == arg || 2 == arg);
+    ptrace_get_string(child->pid, arg, pathname, PATH_MAX);
+
+    if (!(sflags & DONT_RESOLV))
+        rpath = safe_realpath(pathname, child->pid, 1, &issymlink);
+    else
+        rpath = safe_realpath(pathname, child->pid, 0, NULL);
+
+    if (NULL == rpath) {
+        if (ENOENT == errno) {
+            /* File doesn't exist, check the directory */
+            char *dirc, *dname;
+            dirc = xstrndup(pathname, PATH_MAX);
+            dname = dirname(dirc);
+            if (!(sflags & DONT_RESOLV))
+                rpath = safe_realpath(dname, child->pid, 1, &issymlink);
+            else
+                rpath = safe_realpath(dname, child->pid, 0, NULL);
+            free(dirc);
+
+            if (NULL == rpath) {
+                /* Directory doesn't exist as well.
+                 * The system call will fail, to prevent any kind of races
+                 * we deny access without calling it but don't throw an
+                 * access violation.
+                 */
+                decs->res = R_DENY_RETURN;
+                decs->ret = -1;
+                return 0;
+            }
+            else {
+                /* Add the basename back */
+                char *basec, *bname;
+                basec = xstrndup(pathname, PATH_MAX);
+                bname = basename(basec);
+                strncat(rpath, "/", 1);
+                strncat(rpath, bname, strlen(bname));
+                free(basec);
+            }
+        }
+        else {
+            lg(LOG_WARNING, "syscall.syscall_check.fail_safe_realpath",
+                    "safe_realpath() failed for \"%s\": %s", pathname, strerror(errno));
+            decs->res = R_DENY_RETURN;
+            decs->ret = -1;
+            return 0;
+        }
+    }
+
+    if (sflags & ACCESS_MODE) {
+        int mode = ptrace(PTRACE_PEEKUSER, child->pid, PARAM2, NULL);
+        if (0 > mode) {
+            free(rpath);
+            die(EX_SOFTWARE, "PTRACE_PEEKUSER failed: %s", strerror(errno));
+        }
+        if (!(mode & W_OK)) {
+            if (issymlink) {
+                /* Change the pathname argument with the resolved path to
+                 * prevent symlink races.
+                 */
+                ptrace_set_string(child->pid, 1, rpath, PATH_MAX);
+            }
+            free(rpath);
+            decs->res = R_ALLOW;
+            return 0;
+        }
+    }
+    else if (sflags & OPEN_MODE) {
+        int mode = ptrace(PTRACE_PEEKUSER, child->pid, PARAM2, NULL);
+        if (0 > mode) {
+            free(rpath);
+            die(EX_SOFTWARE, "PTRACE_PEEKUSER failed: %s", strerror(errno));
+        }
+        if (!(mode & O_WRONLY || mode & O_RDWR)) {
+            if (issymlink) {
+                /* Change the pathname argument with the resolved path to
+                 * prevent symlink races.
+                 */
+                ptrace_set_string(child->pid, 1, rpath, PATH_MAX);
+            }
+            free(rpath);
+            decs->res = R_ALLOW;
+            return 0;
+        }
+    }
+
+    int allow_write = pathlist_check(&(ctx->write_prefixes), rpath);
+    int allow_predict = pathlist_check(&(ctx->predict_prefixes), rpath);
+
+    if (!allow_write && !allow_predict) {
+        decs->res = R_DENY_VIOLATION;
+        if (1 == arg)
+            snprintf(decs->reason, REASON_MAX, "%s(\"%s\", ", sname,
+                    pathname);
+        else if (2 == arg)
+            snprintf(decs->reason, REASON_MAX, "%s(?, \"%s\", ", sname,
+                    pathname);
+        if (sflags & ACCESS_MODE)
+            strcat(decs->reason, "O_WR)");
+        else if (sflags & OPEN_MODE)
+            strcat(decs->reason, "O_WRONLY/O_RDWR, ...)");
+        else
+            strcat(decs->reason, "...)");
+        free(rpath);
+        return 0;
+    }
+    else if (!allow_write && allow_predict) {
+        if (sflags & RETURNS_FD) {
+            /* Change path argument to /dev/null.
+             */
+            ptrace_set_string(child->pid, arg, "/dev/null", 10);
+            decs->res = R_ALLOW;
+        }
+        else {
+            decs->res = R_DENY_RETURN;
+            decs->ret = 0;
+        }
+        free(rpath);
+        return 0;
+    }
+
+    if (issymlink) {
+        /* Change the pathname argument with the resolved path to
+        * prevent symlink races.
+        */
+        ptrace_set_string(child->pid, arg, rpath, PATH_MAX);
+    }
+    free(rpath);
+    return 0;
+}
+
 struct decision syscall_check(context_t *ctx, struct tchild *child, int syscall) {
     unsigned int sflags, i;
     const char *sname;
@@ -96,134 +252,14 @@ found:
             "Child %i called essential system call %s()", child->pid, sname);
 
     if (sflags & CHECK_PATH) {
-        int issymlink;
-        char pathname[PATH_MAX];
-        char *rpath;
-
-        ptrace_get_string(child->pid, 1, pathname, PATH_MAX);
-
-        if (sflags & RESOLV_PATH)
-            rpath = safe_realpath(pathname, child->pid, 1, &issymlink);
-        else
-            rpath = safe_realpath(pathname, child->pid, 0, NULL);
-
-        if (NULL == rpath) {
-            if (ENOENT == errno) {
-                /* File doesn't exist, check the directory */
-                char *dirc, *dname;
-                dirc = xstrndup(pathname, PATH_MAX);
-                dname = dirname(dirc);
-                if (sflags & RESOLV_PATH)
-                    rpath = safe_realpath(dname, child->pid, 1, &issymlink);
-                else
-                    rpath = safe_realpath(dname, child->pid, 0, NULL);
-                free(dirc);
-
-                if (NULL == rpath) {
-                    /* Directory doesn't exist as well.
-                     * The system call will fail, to prevent any kind of races
-                     * we deny access without calling it but don't throw an
-                     * access violation.
-                     */
-                    decs.res = R_DENY_RETURN;
-                    decs.ret = -1;
-                    return decs;
-                }
-                else {
-                    /* Add the basename back */
-                    char *basec, *bname;
-                    basec = xstrndup(pathname, PATH_MAX);
-                    bname = basename(basec);
-                    strncat(rpath, "/", 1);
-                    strncat(rpath, bname, strlen(bname));
-                    free(basec);
-                }
-            }
-            else {
-                lg(LOG_WARNING, "syscall.syscall_check.fail_safe_realpath",
-                        "safe_realpath() failed for \"%s\": %s", pathname, strerror(errno));
-                decs.res = R_DENY_RETURN;
-                decs.ret = -1;
-                return decs;
-            }
-        }
-
-        if (sflags & ACCESS_MODE) {
-            int mode = ptrace(PTRACE_PEEKUSER, child->pid, PARAM2, NULL);
-            if (0 > mode) {
-                free(rpath);
-                die(EX_SOFTWARE, "PTRACE_PEEKUSER failed: %s", strerror(errno));
-            }
-            if (!(mode & W_OK)) {
-                if (issymlink) {
-                    /* Change the pathname argument with the resolved path to
-                     * prevent symlink races.
-                     */
-                    ptrace_set_string(child->pid, 1, rpath, PATH_MAX);
-                }
-                free(rpath);
-                decs.res = R_ALLOW;
-                return decs;
-            }
-        }
-        else if (sflags & OPEN_MODE) {
-            int mode = ptrace(PTRACE_PEEKUSER, child->pid, PARAM2, NULL);
-            if (0 > mode) {
-                free(rpath);
-                die(EX_SOFTWARE, "PTRACE_PEEKUSER failed: %s", strerror(errno));
-            }
-            if (!(mode & O_WRONLY || mode & O_RDWR)) {
-                if (issymlink) {
-                    /* Change the pathname argument with the resolved path to
-                     * prevent symlink races.
-                     */
-                    ptrace_set_string(child->pid, 1, rpath, PATH_MAX);
-                }
-                free(rpath);
-                decs.res = R_ALLOW;
-                return decs;
-            }
-        }
-
-        int allow_write = pathlist_check(&(ctx->write_prefixes), rpath);
-        int allow_predict = pathlist_check(&(ctx->predict_prefixes), rpath);
-
-        if (!allow_write && !allow_predict) {
-            decs.res = R_DENY_VIOLATION;
-            snprintf(decs.reason, REASON_MAX, "%s(\"%s\", ", sname,
-                    pathname);
-            if (sflags & ACCESS_MODE)
-                strcat(decs.reason, "O_WR)");
-            else if (sflags & OPEN_MODE)
-                strcat(decs.reason, "O_WRONLY/O_RDWR, ...)");
-            else
-                strcat(decs.reason, "...)");
-            free(rpath);
+        syscall_check_path(ctx, child, &decs, 1, sflags, sname);
+        if (R_ALLOW != decs.res)
             return decs;
-        }
-        else if (!allow_write && allow_predict) {
-            if (sflags & RETURNS_FD) {
-                /* Change path argument to /dev/null.
-                 * TODO: remove O_CREAT from flags if OPEN_MODE is set.
-                 */
-                ptrace_set_string(child->pid, 1, "/dev/null", 10);
-                decs.res = R_ALLOW;
-            }
-            else {
-                decs.res = R_DENY_RETURN;
-                decs.ret = 0;
-            }
-            free(rpath);
+    }
+    if (sflags & CHECK_PATH2) {
+        syscall_check_path(ctx, child, &decs, 2, sflags, sname);
+        if (R_ALLOW != decs.res)
             return decs;
-        }
-
-        if (issymlink) {
-            /* Change the pathname argument with the resolved path to
-            * prevent symlink races.
-            */
-            ptrace_set_string(child->pid, 1, rpath, PATH_MAX);
-        }
-        free(rpath);
     }
     if (sflags & NET_CALL && !(ctx->net_allowed)) {
         decs.res = R_DENY_VIOLATION;
