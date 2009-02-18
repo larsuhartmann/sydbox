8d04b8bf62d9e7d5af495d9b64abaa19e9d4704c
Ali Polatel <polatel@gmail.com>
2009-02-18 17:02:25 +0000

    Add tests for link

---
+++ tests/t06-link.bash
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+# vim: set sw=4 et sts=4 tw=80 :
+# Copyright 2009 Ali Polatel <polatel@gmail.com>
+# Distributed under the terms of the GNU General Public License v2
+
+. test-lib.bash
+
+rm -f arnold.layne
+
+say "t06-link-deny"
+sydbox -- ./t06_link
+if [[ 0 == $? ]]; then
+    die "failed to deny link"
+fi
+
+say "t06-link-predict"
+SANDBOX_PREDICT="${cwd}" sydbox -- ./t06_link
+if [[ 0 != $? ]]; then
+    die "failed to predict link"
+fi
+
+say "t06-link-write"
+SANDBOX_WRITE="${cwd}" sydbox -- ./t06_link
+if [[ 0 != $? ]]; then
+    die "write didn't allow access"
+elif [[ ! -f arnold.layne ]]; then
+    die "file doesn't exist, write didn't allow access"
+fi
---
+++ tests/t06-link.c
@@ -0,0 +1,16 @@
+/* Check program for t06-link.bash
+ * vim: set et ts=4 sts=4 sw=4 fdm=syntax :
+ * Copyright 2009 Ali Polatel <polatel@gmail.com>
+ * Distributed under the terms of the GNU General Public License v2
+ */
+
+#include <errno.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(void) {
+    if (0 > link("t06-link.bash", "arnold.layne"))
+        return EXIT_FAILURE;
+    else
+        return EXIT_SUCCESS;
+}
