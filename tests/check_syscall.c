b436f18dca27aaa944887e6dcb23840e7aaedc58
Ali Polatel <polatel@gmail.com>
2009-02-24 16:09:16 +0000

    Call pause() at the end of child to ensure child doesn't die before we check it, add tests for creat

--- tests/check_syscall.c
+++ tests/check_syscall.c
@@ -35,6 +35,7 @@ START_TEST(check_syscall_check_chmod_deny) {
         ptrace(PTRACE_TRACEME, 0, NULL, NULL);
         kill(getpid(), SIGSTOP);
         chmod("/dev/null", 0755);
+        pause();
     }
     else { /* parent */
         int status;
@@ -78,6 +79,7 @@ START_TEST(check_syscall_check_chmod_predict) {
         ptrace(PTRACE_TRACEME, 0, NULL, NULL);
         kill(getpid(), SIGSTOP);
         chmod("/dev/null", 0755);
+        pause();
     }
     else { /* parent */
         int status;
@@ -125,6 +127,7 @@ START_TEST(check_syscall_check_chmod_allow) {
         ptrace(PTRACE_TRACEME, 0, NULL, NULL);
         kill(getpid(), SIGSTOP);
         chmod("/dev/null", 0755);
+        pause();
     }
     else { /* parent */
         int status;
@@ -170,6 +173,7 @@ START_TEST(check_syscall_check_chown_deny) {
         ptrace(PTRACE_TRACEME, 0, NULL, NULL);
         kill(getpid(), SIGSTOP);
         chown("/dev/null", 0, 0);
+        pause();
     }
     else { /* parent */
         int status;
@@ -214,6 +218,7 @@ START_TEST(check_syscall_check_chown_predict) {
         ptrace(PTRACE_TRACEME, 0, NULL, NULL);
         kill(getpid(), SIGSTOP);
         chown("/dev/null", 0, 0);
+        pause();
     }
     else { /* parent */
         int status;
@@ -261,6 +266,7 @@ START_TEST(check_syscall_check_chown_allow) {
         ptrace(PTRACE_TRACEME, 0, NULL, NULL);
         kill(getpid(), SIGSTOP);
         chown("/dev/null", 0, 0);
+        pause();
     }
     else { /* parent */
         int status;
@@ -306,6 +312,7 @@ START_TEST(check_syscall_check_open_rdonly_allow) {
         ptrace(PTRACE_TRACEME, 0, NULL, NULL);
         kill(getpid(), SIGSTOP);
         open("/dev/null", O_RDONLY);
+        pause();
     }
     else { /* parent */
         int status;
@@ -350,6 +357,7 @@ START_TEST(check_syscall_check_open_wronly_deny) {
         ptrace(PTRACE_TRACEME, 0, NULL, NULL);
         kill(getpid(), SIGSTOP);
         open("/dev/null", O_WRONLY);
+        pause();
     }
     else { /* parent */
         int status;
@@ -480,6 +488,7 @@ START_TEST(check_syscall_check_open_wronly_allow) {
         ptrace(PTRACE_TRACEME, 0, NULL, NULL);
         kill(getpid(), SIGSTOP);
         open("/dev/null", O_WRONLY);
+        pause();
     }
     else { /* parent */
         int status;
@@ -525,6 +534,7 @@ START_TEST(check_syscall_check_open_rdwr_deny) {
         ptrace(PTRACE_TRACEME, 0, NULL, NULL);
         kill(getpid(), SIGSTOP);
         open("/dev/null", O_RDWR);
+        pause();
     }
     else { /* parent */
         int status;
@@ -655,6 +665,7 @@ START_TEST(check_syscall_check_open_rdwr_allow) {
         ptrace(PTRACE_TRACEME, 0, NULL, NULL);
         kill(getpid(), SIGSTOP);
         open("/dev/null", O_RDWR);
+        pause();
     }
     else { /* parent */
         int status;
@@ -690,6 +701,195 @@ START_TEST(check_syscall_check_open_rdwr_allow) {
 }
 END_TEST
 
+START_TEST(check_syscall_check_creat_deny) {
+    pid_t pid;
+
+    pid = fork();
+    if (0 > pid)
+        fail("fork() failed: %s", strerror(errno));
+    else if (0 == pid) { /* child */
+        ptrace(PTRACE_TRACEME, 0, NULL, NULL);
+        kill(getpid(), SIGSTOP);
+        creat("emily/syd.txt", 0644);
+        pause();
+    }
+    else { /* parent */
+        int status;
+        long syscall;
+        context_t *ctx = context_new();
+
+        tchild_new(&(ctx->children), pid);
+        ctx->eldest = ctx->children;
+        wait(&status);
+        fail_unless(WIFSTOPPED(status),
+                "child %i didn't stop by sending itself SIGSTOP",
+                pid);
+        fail_unless(0 == trace_setup(pid),
+                "Failed to set tracing options: %s", strerror(errno));
+
+        /* Resume the child, it will stop at the next system call. */
+        fail_unless(0 == ptrace(PTRACE_SYSCALL, pid, NULL, NULL),
+                "PTRACE_SYSCALL failed: %s", strerror(errno));
+        wait(&status);
+        fail_unless(WIFSTOPPED(status),
+                "child %i didn't stop by sending itself SIGTRAP",
+                pid);
+
+        fail_if(0 > trace_get_syscall(pid, &syscall),
+                "Failed to get syscall: %s", strerror(errno));
+        fail_if(syscall_check(ctx, ctx->eldest, syscall),
+                "Allowed access, expected violation");
+
+        kill(pid, SIGTERM);
+    }
+}
+END_TEST
+
+START_TEST(check_syscall_check_creat_predict) {
+    pid_t pid;
+    int pfd[2];
+    char cwd[PATH_MAX];
+    char *rcwd;
+
+    /* setup */
+    if (NULL == getcwd(cwd, PATH_MAX))
+        fail("getcwd failed: %s", strerror(errno));
+    rcwd = realpath(cwd, NULL);
+    if (NULL == rcwd)
+        fail("realpath failed: %s", strerror(errno));
+    if (0 > mkdir("emily", 0755))
+        fail("mkdir failed: %s", strerror(errno));
+
+    if (0 > pipe(pfd))
+        fail("pipe() failed: %s", strerror(errno));
+    pid = fork();
+    if (0 > pid)
+        fail("fork() failed: %s", strerror(errno));
+    else if (0 == pid) { /* child */
+        ptrace(PTRACE_TRACEME, 0, NULL, NULL);
+        kill(getpid(), SIGSTOP);
+
+        char buf[16];
+        snprintf(buf, 16, "%d", creat("emily/syd.txt", 0644));
+        write(pfd[1], buf, 16);
+        pause();
+    }
+    else { /* parent */
+        int status;
+        long syscall;
+        context_t *ctx = context_new();
+
+        close(pfd[1]);
+
+        pathnode_new(&(ctx->predict_prefixes), rcwd);
+        tchild_new(&(ctx->children), pid);
+        ctx->eldest = ctx->children;
+
+        wait(&status);
+        fail_unless(WIFSTOPPED(status),
+                "child %i didn't stop by sending itself SIGSTOP",
+                pid);
+        fail_unless(0 == trace_setup(pid),
+                "Failed to set tracing options: %s", strerror(errno));
+
+        /* Resume the child, it will stop at the next system call. */
+        fail_unless(0 == ptrace(PTRACE_SYSCALL, pid, NULL, NULL),
+                "PTRACE_SYSCALL failed: %s", strerror(errno));
+        wait(&status);
+        fail_unless(WIFSTOPPED(status),
+                "child %i didn't stop by sending itself SIGTRAP",
+                pid);
+
+        fail_if(0 > trace_get_syscall(pid, &syscall),
+                "Failed to get syscall: %s", strerror(errno));
+        fail_unless(syscall_check(ctx, ctx->eldest, syscall),
+                "Denied access, expected allow");
+
+        /* Resume the child so it writes to the pipe */
+        fail_unless(0 == ptrace(PTRACE_CONT, pid, NULL, NULL),
+                "PTRACE_CONT failed: %s", strerror(errno));
+
+        int fd, n;
+        char buf[16], proc[PATH_MAX], rpath[PATH_MAX];
+
+        if (0 > read(pfd[0], buf, 16))
+            fail("read() failed: %s", strerror(errno));
+        fd = atoi(buf);
+
+        snprintf(proc, PATH_MAX, "/proc/%i/fd/%i", pid, fd);
+        n = readlink(proc, rpath, PATH_MAX);
+        if (0 > n)
+            fail("readlink failed: %s", strerror(errno));
+        proc[n] = '\0';
+
+        if (0 != strncmp(rpath, "/dev/null", 10))
+            fail("expected /dev/null got \"%s\"", rpath);
+
+        kill(pid, SIGTERM);
+    }
+}
+END_TEST
+
+START_TEST(check_syscall_check_creat_allow) {
+    pid_t pid;
+    char cwd[PATH_MAX];
+    char *rcwd;
+
+    /* setup */
+    if (NULL == getcwd(cwd, PATH_MAX))
+        fail("getcwd failed: %s", strerror(errno));
+    rcwd = realpath(cwd, NULL);
+    if (NULL == rcwd)
+        fail("realpath failed: %s", strerror(errno));
+    if (0 > mkdir("emily", 0755))
+        fail("mkdir failed: %s", strerror(errno));
+
+    pid = fork();
+    if (0 > pid)
+        fail("fork() failed: %s", strerror(errno));
+    else if (0 == pid) { /* child */
+        ptrace(PTRACE_TRACEME, 0, NULL, NULL);
+        kill(getpid(), SIGSTOP);
+        creat("emily/syd.txt", 0666);
+        pause();
+    }
+    else { /* parent */
+        int status;
+        long syscall;
+        struct stat buf;
+        context_t *ctx = context_new();
+
+        pathlist_init(&(ctx->write_prefixes), rcwd);
+        tchild_new(&(ctx->children), pid);
+        ctx->eldest = ctx->children;
+
+        wait(&status);
+        fail_unless(WIFSTOPPED(status),
+                "child %i didn't stop by sending itself SIGSTOP",
+                pid);
+        fail_unless(0 == trace_setup(pid),
+                "Failed to set tracing options: %s", strerror(errno));
+
+        /* Resume the child, it will stop at the next system call. */
+        fail_unless(0 == ptrace(PTRACE_SYSCALL, pid, NULL, NULL),
+                "PTRACE_SYSCALL failed: %s", strerror(errno));
+        wait(&status);
+        fail_unless(WIFSTOPPED(status),
+                "child %i didn't stop by sending itself SIGTRAP",
+                pid);
+
+        fail_if(0 > trace_get_syscall(pid, &syscall),
+                "Failed to get syscall: %s", strerror(errno));
+        fail_unless(syscall_check(ctx, ctx->eldest, syscall),
+                "Denied access, expected allow");
+        fail_unless(0 > stat("emily/syd.txt", &buf),
+                "Allowed access but file doesn't exist: %s", strerror(errno));
+        unlink("emily/syd.txt");
+        kill(pid, SIGTERM);
+    }
+}
+END_TEST
+
 Suite *syscall_suite_create(void) {
     Suite *s = suite_create("syscall");
 
@@ -709,6 +909,9 @@ Suite *syscall_suite_create(void) {
     tcase_add_test(tc_syscall_check, check_syscall_check_open_rdwr_deny);
     tcase_add_test(tc_syscall_check, check_syscall_check_open_rdwr_predict);
     tcase_add_test(tc_syscall_check, check_syscall_check_open_rdwr_allow);
+    tcase_add_test(tc_syscall_check, check_syscall_check_creat_deny);
+    tcase_add_test(tc_syscall_check, check_syscall_check_creat_predict);
+    tcase_add_test(tc_syscall_check, check_syscall_check_creat_allow);
     suite_add_tcase(s, tc_syscall_check);
 
     return s;
