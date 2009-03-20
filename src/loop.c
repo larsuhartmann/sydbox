50e09bec8c3dad42206fb82a84155b3a9e805c76
Ali Polatel <polatel@gmail.com>
2009-03-20 12:48:47 +0200

    nanosleep for a certain interval between waitpid calls so sydbox doesn't eat all cpu

--- src/loop.c
+++ src/loop.c
@@ -25,8 +25,11 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <time.h>
 
 #include "defs.h"
+
+const struct timespec waitpid_interval = { .tv_sec = 0, .tv_nsec = 32 };
 
 // Event handlers
 static int xsetup(context_t *ctx, struct tchild *child) {
@@ -139,8 +142,10 @@ int trace_loop(context_t *ctx) {
     ret = EXIT_SUCCESS;
     while (NULL != ctx->children) {
         pid = waitpid(-1, &status, __WALL | WNOHANG);
-        if (likely(0 == pid))
+        if (likely(0 == pid)) {
+            nanosleep(&waitpid_interval, NULL);
             continue;
+        }
         else if (unlikely(0 > pid)) {
             LOGE("waitpid failed: %s", strerror(errno));
             DIESOFT("waitpid failed: %s", strerror(errno));
