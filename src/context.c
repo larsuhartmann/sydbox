34842029e61329365bf026e0ae6664986b29e9e2
Saleem Abdulrasool <compnerd@compnerd.org>
2009-04-05 16:18:54 -0700

    #include love, convert paranoid to boolean

--- src/context.c
+++ src/context.c
@@ -18,35 +18,42 @@
  */
 
 #include <errno.h>
-#include <string.h>
-#include <sysexits.h>
+#include <stdlib.h>
 
 #include <glib.h>
 
-#include "defs.h"
 #include "getcwd.h"
-#include "util.h"
 #include "context.h"
 #include "children.h"
 
-context_t *context_new(void) {
+context_t *
+context_new (void)
+{
     context_t *ctx;
-    ctx = (context_t *) g_malloc (sizeof(context_t));
-    ctx->paranoid = 0;
-    ctx->cwd = egetcwd();
-    if (NULL == ctx->cwd) {
+
+    ctx = (context_t *) g_malloc (sizeof (context_t));
+
+    ctx->eldest = NULL;
+    ctx->children = NULL;
+    ctx->paranoid = FALSE;
+
+    if (! (ctx->cwd = egetcwd())) {
         g_printerr ("failed to get current working directory: %s", g_strerror (errno));
         exit (-1);
     }
-    ctx->children = NULL;
-    ctx->eldest = NULL;
+
     return ctx;
 }
 
-void context_free(context_t *ctx) {
+void
+context_free (context_t *ctx)
+{
     if (NULL != ctx->cwd)
         g_free (ctx->cwd);
+
     if (NULL != ctx->children)
-        tchild_free(&(ctx->children));
+        tchild_free (&(ctx->children));
+
     g_free (ctx);
 }
+
--- src/context.h
+++ src/context.h
@@ -3,6 +3,19 @@
 /**
  * Copyright 2009 Saleem Abdulrasool <compnerd@compnerd.org>
  * Copyright 2009 Ali Polatel <polatel@gmail.com>
+ *
+ * This file is part of the sydbox sandbox tool. sydbox is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License version 2, as published by the Free Software Foundation.
+ *
+ * sydbox is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA  02111-1307  USA
  **/
 
 #ifndef __CONTEXT_H__
@@ -12,7 +25,7 @@
 
 typedef struct
 {
-    int paranoid;
+    gboolean paranoid;
     char *cwd;              // current working directory
     struct tchild *eldest;  // first child is kept to determine return code
     GSList *children;       // list of children
