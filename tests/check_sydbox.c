771982ded8ffcfcb82fa03a33476162baa5acea9
Ali Polatel <polatel@gmail.com>
2009-04-08 01:52:26 +0300

    Fix tests, remove check_syscall.c because that tests cases are already covered by progtests.

--- tests/check_sydbox.c
+++ tests/check_sydbox.c
@@ -23,7 +23,6 @@ int main(void) {
     srunner_add_suite(sr, path_suite_create());
     srunner_add_suite(sr, children_suite_create());
     srunner_add_suite(sr, trace_suite_create());
-    srunner_add_suite(sr, syscall_suite_create());
 
     /* Run and grab the results */
     srunner_run_all(sr, CK_VERBOSE);
