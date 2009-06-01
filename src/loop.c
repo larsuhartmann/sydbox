larsuhartmann/sydbox

commit 77efe5b6ddf7ddb7a86999cb5787b9450d64300c

remove premature setup crap, fix trace_event()

src/loop.c
@@ -61,22 +61,6 @@ static int xsetup(context_t *ctx, struct tchild *child) {
     return 0;
 }
 
-static int xsetup_premature(context_t *ctx, pid_t pid) {
-    tchild_new(&(ctx->children), pid);
-    /* XXX: Shameless hack
-     * We need to update the current working directory here,
-     * because inheritance doesn't work for prematurely borned children.
-     */
-    struct tchild *child = ctx->children->data;
-    g_free(child->cwd);
-    child->cwd = pgetcwd(ctx, child->pid);
-    if (NULL == child->cwd) {
-        g_printerr("failed to determine current working directory of prematurely born child %d", child->pid);
-        exit(-1);
-    }
-    return xsetup(ctx, ctx->children->data);
-}
-
 static int xsyscall(context_t *ctx, struct tchild *child) {
     if (0 > trace_syscall(child->pid, 0)) {
         if (errno != ESRCH) {
@@ -105,22 +89,21 @@ static int xfork(context_t *ctx, struct tchild *child) {
         g_debug ("the newborn child's pid is %i", childpid);
 
     newchild = tchild_find(ctx->children, childpid);
-    if (NULL != newchild) {
-        g_debug ("child %i is prematurely born, letting it continue its life", newchild->pid);
-        if (0 > trace_syscall(newchild->pid, 0)) {
-            if (errno != ESRCH) {
-                g_printerr ("failed to resume prematurely born child %i: %s",
-                        newchild->pid, g_strerror (errno));
-                exit (-1);
-            }
-            return context_remove_child (ctx, newchild->pid);
+    if (NULL == newchild) {
+        // Add the child, setup has already been done.
+        tchild_new(&(ctx->children), childpid, child->pid);
+    }
+
+    if (0 > trace_syscall(childpid, 0)) {
+        if (ESRCH != errno) {
+            g_printerr("failed to resume new born child %i", childpid);
+            exit(-1);
         }
-        g_log (G_LOG_DOMAIN, LOG_LEVEL_DEBUG_TRACE, "resumed prematurely born child %i", newchild->pid);
+        if (NULL != newchild)
+            return context_remove_child(ctx, childpid);
     }
-    else {
-        // Add the child, setup will be done later
-        tchild_new(&(ctx->children), childpid);
-    }
+    g_debug("resumed new born child %i", childpid);
+
     return xsyscall(ctx, child);
 }
 
@@ -139,8 +122,10 @@ static int xgenuine(context_t * ctx, struct tchild *child, int status) {
 static int xunknown(context_t *ctx, struct tchild *child, int status) {
     if (0 > trace_syscall(child->pid, WSTOPSIG(status))) {
         if (errno != ESRCH) {
-            g_critical ("failed to resume child %i after unknown signal %#x: %s", child->pid, status, g_strerror (errno));
-            g_printerr ("failed to resume child %i after unknown signal %#x: %s", child->pid, status, g_strerror (errno));
+            g_critical ("failed to resume child %i after unknown signal %#x: %s",
+                    child->pid, status, g_strerror (errno));
+            g_printerr ("failed to resume child %i after unknown signal %#x: %s",
+                    child->pid, status, g_strerror (errno));
             exit (-1);
         }
         return context_remove_child (ctx, child->pid);
@@ -171,13 +156,21 @@ int trace_loop(context_t *ctx) {
             case E_STOP:
                 g_debug ("latest event for child %i is E_STOP, calling event handler", pid);
                 if (NULL == child) {
-                    g_debug("child %d has born prematurely", pid);
-                    ret = xsetup_premature(ctx, pid);
-                    if (0 != ret)
-                        return ret;
+                    /* Child is born before PTRACE_EVENT_FORK, setup the child
+                     * but don't resume it until we receive the event.
+                     */
+                    g_debug("setting up prematurely born child %i", pid);
+                    if (0 > trace_setup(pid)) {
+                        if (ESRCH != errno) {
+                            g_printerr("failed to set up prematurely born child %i", pid);
+                            exit(-1);
+                        }
+                        else
+                            g_debug("failed to set up prematurely born child %i: %s", pid, g_strerror(errno));
+                    }
                 }
                 else {
-                    g_debug("setting up child %d", child->pid);
+                    g_debug("setting up child %i", child->pid);
                     ret = xsetup(ctx, child);
                     if (0 != ret)
                         return ret;

src/children.c
@@ -19,6 +19,7 @@
  * Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+#include <assert.h>
 #include <errno.h>
 #include <stdlib.h>
 #include <string.h>
@@ -29,7 +30,7 @@
 #include "children.h"
 #include "sydbox-log.h"
 
-void tchild_new(GSList **children, pid_t pid) {
+void tchild_new(GSList **children, pid_t pid, pid_t ppid) {
     struct tchild *child, *parent;
 
     g_debug ("new child %i", pid);
@@ -47,8 +48,9 @@ void tchild_new(GSList **children, pid_t pid) {
     child->sandbox->predict_prefixes = NULL;
 
     // Inheritance
-    if (NULL != *children && NULL != (*children)->data) {
-        parent = (*children)->data;
+    if (0 < ppid) {
+        parent = tchild_find(*children, ppid);
+        assert(NULL != parent);
 
         if (NULL != parent->cwd) {
             g_debug ("child %i inherits parent %i's current working directory '%s'", pid, parent->pid, parent->cwd);

src/children.h
@@ -59,7 +59,7 @@ struct tchild
 };
 
 void
-tchild_new (GSList **children, pid_t pid);
+tchild_new (GSList **children, pid_t pid, pid_t ppid);
 
 void
 tchild_free (GSList **children);

src/main.c
@@ -214,7 +214,7 @@ sydbox_execute_parent (int argc G_GNUC_UNUSED, char **argv G_GNUC_UNUSED, pid_t 
         exit (-1);
     }
 
-    tchild_new (&(ctx->children), pid);
+    tchild_new (&(ctx->children), pid, -1);
     ctx->eldest = pid;
     eldest = tchild_find(ctx->children, pid);
     eldest->cwd = g_strdup (ctx->cwd);

src/trace.c
@@ -129,21 +129,25 @@ static int umovestr(pid_t pid, long addr, char *dest, size_t len) {
 }
 
 unsigned int trace_event(int status) {
+    int sig;
+    unsigned int event;
+
     if (WIFSTOPPED(status)) {
-        int sig = WSTOPSIG(status);
+        sig = WSTOPSIG(status);
         if (SIGSTOP == sig)
             return E_STOP;
         else if ((SIGTRAP | 0x80) == sig)
             return E_SYSCALL;
 
-        switch (status) {
-            case SIGTRAP | PTRACE_EVENT_FORK << 8:
+        event = (status >> 16) & 0xffff;
+        switch (event) {
+            case PTRACE_EVENT_FORK:
                 return E_FORK;
-            case SIGTRAP | PTRACE_EVENT_VFORK << 8:
+            case PTRACE_EVENT_VFORK:
                 return E_VFORK;
-            case SIGTRAP | PTRACE_EVENT_CLONE << 8:
+            case PTRACE_EVENT_CLONE:
                 return E_CLONE;
-            case SIGTRAP | PTRACE_EVENT_EXEC << 8:
+            case PTRACE_EVENT_EXEC:
                 return E_EXEC;
             default:
                 return E_GENUINE;

tests/check_children.c
@@ -24,7 +24,7 @@ START_TEST(check_tchild_new) {
     GSList *tc = NULL;
     struct tchild *child;
 
-    tchild_new(&tc, 666);
+    tchild_new(&tc, 666, -1);
 
     fail_unless(NULL != tc);
     child = (struct tchild *) tc->data;
@@ -43,7 +43,7 @@ START_TEST(check_tchild_free) {
     PRINT_TEST_HEADER;
     GSList *tc = NULL;
 
-    tchild_new(&tc, 666);
+    tchild_new(&tc, 666, -1);
     tchild_free(&tc);
 
     fail_unless(NULL == tc);
@@ -54,7 +54,7 @@ START_TEST(check_tchild_delete_first) {
     PRINT_TEST_HEADER;
     GSList *tc = NULL;
 
-    tchild_new(&tc, 666);
+    tchild_new(&tc, 666, -1);
     tchild_delete(&tc, 666);
 
     fail_unless(NULL == tc);
@@ -67,9 +67,9 @@ START_TEST(check_tchild_delete) {
     GSList *tc = NULL;
     GSList *walk = NULL;
 
-    tchild_new(&tc, 666);
-    tchild_new(&tc, 667);
-    tchild_new(&tc, 668);
+    tchild_new(&tc, 666, -1);
+    tchild_new(&tc, 667, 666);
+    tchild_new(&tc, 668, 667);
 
     tchild_delete(&tc, 666);
 

commit a14b455649ff98b5e8bd77ed9fe57332db0c0449

fix a bug introduced by 77efe5b

src/loop.c
@@ -105,7 +105,7 @@ static int xfork(context_t *ctx, struct tchild *child) {
             /* Child isn't prematurely born, and it's dead (rip).
              * Remove it from the list of children.
              */
-            return context_remove_child(ctx, childpid);
+            context_remove_child(ctx, childpid);
         }
     }
     g_debug("resumed new born child %i", childpid);