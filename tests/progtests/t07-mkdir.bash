46a20921ba09f4fb747f6fec274116fb8b4b02c4
Ali Polatel <polatel@gmail.com>
2009-06-03 22:42:58 +0300

    too long path tests for mkdir()

--- tests/progtests/t07-mkdir.bash
+++ tests/progtests/t07-mkdir.bash
@@ -34,3 +34,32 @@ elif [[ ! -d see.emily.play ]]; then
     die "dir doesn't exist, write didn't allow access"
 fi
 end_test
+
+# Tests dealing with too long paths
+dname="see.emily.play"
+tmpfile="$(mkdir_long)"
+
+start_test "t07-mkdir-toolong-deny"
+sydbox -- ./t07_mkdir_toolong "$long_dir" "$dname"
+if [[ 0 == $? ]]; then
+    die "failed to deny mkdir"
+elif stat_long "$dname"; then
+    die "failed to deny mkdir, dir exists"
+fi
+
+start_test "t07-mkdir-toolong-predict"
+SANDBOX_PREDICT="$cwd"/$long_dir sydbox -- ./t07_mkdir_toolong "$long_dir" "$dname"
+if [[ 0 != $? ]]; then
+    die "failed to predict mkdir"
+elif stat_long "$dname"; then
+    die "predict allowed access"
+fi
+
+start_test "t07-mkdir-toolong-write"
+SANDBOX_WRITE="$cwd"/$long_dir sydbox -- ./t07_mkdir_toolong "$long_dir" "$dname"
+if [[ 0 != $? ]]; then
+    die "failed to allow mkdir"
+elif ! stat_long "$dname"; then
+    die "failed to allow mkdir, dir doesn't exist"
+fi
+end_test
---
+++ tests/progtests/t07_mkdir_toolong.c
@@ -0,0 +1,32 @@
+/* Check program for t07-mkdir.bash
+ * vim: set et ts=4 sts=4 sw=4 fdm=syntax :
+ * Copyright 2009 Ali Polatel <polatel@gmail.com>
+ * Distributed under the terms of the GNU General Public License v2
+ */
+
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(int argc, char **argv) {
+    char *long_dir, *fname;
+
+    if (3 > argc)
+        return EXIT_FAILURE;
+    else {
+        long_dir = argv[1];
+        fname = argv[2];
+    }
+
+    for (int i = 0; i < 64; i++) {
+        if (0 > chdir(long_dir))
+            return EXIT_FAILURE;
+    }
+
+
+    if (0 > mkdir(fname, 0644))
+        return EXIT_FAILURE;
+    else
+        return EXIT_SUCCESS;
+}
