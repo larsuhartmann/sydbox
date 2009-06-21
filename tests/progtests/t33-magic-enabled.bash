7a3f8c62b8f2b6d72253941cf5f2cf9dd207767a
Ali Polatel <polatel@gmail.com>
2009-06-22 02:41:13 +0300

    Add tests for /dev/sydbox/enabled

---
+++ tests/progtests/t33-magic-enabled.bash
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+# vim: set sw=4 et sts=4 tw=80 :
+# Copyright 2009 Ali Polatel <polatel@gmail.com>
+# Distributed under the terms of the GNU General Public License v2
+
+no_create_files=1
+. test-lib.bash
+
+start_test "t33-magic-enabled-on"
+sydbox -- bash <<EOF
+[[ -e /dev/sydbox/enabled ]]
+EOF
+if [[ 0 != $? ]]; then
+    die "/dev/sydbox/enabled doesn't exist when sydbox is on"
+fi
+end_test
+
+start_test "t33-magic-enabled-off"
+sydbox -- bash <<EOF
+:>/dev/sydbox/off
+[[ -e /dev/sydbox/enabled ]]
+EOF
+if [[ 0 == $? ]]; then
+    die "/dev/sydbox/enabled exists when sydbox is off"
+fi
+end_test
