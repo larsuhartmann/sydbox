53bc5149d0b13acd109b83fc69b2813f1e00769a
Ali Polatel <polatel@gmail.com>
2009-06-22 03:04:25 +0300

    Add tests for /dev/sydbox/write

---
+++ tests/progtests/t36-magic-write.bash
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+# vim: set sw=4 et sts=4 tw=80 :
+# Copyright 2009 Ali Polatel <polatel@gmail.com>
+# Distributed under the terms of the GNU General Public License v2
+
+. test-lib.bash
+
+start_test "t36-magic-write-stat"
+sydbox -- bash <<EOF
+[[ -e /dev/sydbox/write ]]
+EOF
+if [[ 0 != $? ]]; then
+    die "/dev/sydbox/write doesn't exist"
+fi
+end_test
+
+start_test "t36-magic-write-locked"
+sydbox --lock -- bash <<EOF
+[[ -e /dev/sydbox/write ]]
+EOF
+if [[ 0 == $? ]]; then
+    die "/dev/sydbox/write exists"
+fi
+end_test
+
+start_test "t36-magic-write-add"
+sydbox -- bash <<EOF
+:>/dev/sydbox/write/${cwd}
+echo Oh Arnold Layne, its not the same > arnold.layne
+EOF
+if [[ 0 != $? ]]; then
+    die "failed to add prefix using /dev/sydbox/write"
+elif [[ -z "$(< arnold.layne)" ]]; then
+    die "file empty, failed to add prefix using /dev/sydbox/write"
+fi
+end_test
