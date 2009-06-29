5831cfa9776b24d0665601f237516df4feec3554
Ali Polatel <polatel@gmail.com>
2009-06-29 17:16:18 +0300

    Remove useless include context.h, update header

--- src/proc.h
+++ src/proc.h
@@ -1,13 +1,25 @@
-/* vim: set sw=4 sts=4 fdm=syntax et : */
+/* vim: set sw=4 sts=4 et foldmethod=syntax : */
 
-/**
- * Copyright 2009 Saleem Abdulrasool <compnerd@compnerd.org>
- **/
+/*
+ * Copyright (c) 2009 Saleem Abdulrasool <compnerd@compnerd.org>
+ * Copyright (c) 2009 Ali Polatel <polatel@gmail.com>
+ *
+ * This file is part of the sydbox sandbox tool. sydbox is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License version 2, as published by the Free Software Foundation.
+ *
+ * sydbox is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 
 #ifndef __PROC_H__
 #define __PROC_H__
-
-#include "context.h"
 
 char *
 pgetcwd (pid_t pid);
