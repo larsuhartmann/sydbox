1107a5d8e3d6934dbfbe67726e53706b9dc09ea1
Ali Polatel <polatel@gmail.com>
2009-08-18 16:43:52 +0300

    Revert "fix compiler warning"
    This reverts commit 82a0b32eea70a6eee2f7f558f07af35d0d31ee37.

--- src/trace.c
+++ src/trace.c
@@ -157,9 +157,9 @@ static int trace_ia64_peek(pid_t pid, int narg, long *res)
 #define ACCUM_FLAGS (sizeof(unsigned long) * PT_CCR)
 #define SO_MASK     0x10000000
 static const long syscall_args[1][MAX_ARGS] = {
-    (sizeof(unsigned long) * PT_R3), (sizeof(unsigned long) * PT_R4),
-    (sizeof(unsigned long) * PT_R5), (sizeof(unsigned long) * PT_R6),
-    (sizeof(unsigned long) * PT_R7), (sizeof(unsigned long) * PT_R8)};
+    sizeof(unsigned long) * PT_R3, sizeof(unsigned long) * PT_R4,
+    sizeof(unsigned long) * PT_R5, sizeof(unsigned long) * PT_R6,
+    sizeof(unsigned long) * PT_R7, sizeof(unsigned long) * PT_R8};
 #endif
 
 #define MIN(a,b)        (((a) < (b)) ? (a) : (b))
