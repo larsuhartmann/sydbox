d360f855bd11e847d51adcb1299c4e34427c3e57
Ali Polatel <polatel@gmail.com>
2009-08-27 23:52:37 +0300

    Fix children tests
    Remove some useless tests that doesn't make sense anymore now that we're
    using hash tables.

--- tests/unit/test-children.c
+++ tests/unit/test-children.c
@@ -23,75 +23,46 @@
 #include <children.h>
 #include <sydbox-config.h>
 
-static void
-test1 (void)
+static void test1(void)
 {
-    GSList *children = NULL;
+    GHashTable *children = g_hash_table_new_full(g_direct_hash, g_direct_equal, NULL, tchild_free_one);
     struct tchild *child;
 
-    tchild_new (&children, 666);
-    g_assert (children);
+    tchild_new(children, 666);
 
-    child = (struct tchild *) children->data;
-    g_assert (child);
-    g_assert_cmpint (child->pid, ==, 666);
-    g_assert (child->flags & TCHILD_NEEDSETUP);
-    g_assert (!(child->flags & TCHILD_INSYSCALL));
-    g_assert_cmpint (child->sno, ==, 0xbadca11);
-    g_assert_cmpint (child->retval, ==, -1);
+    child = (struct tchild *) tchild_find(children, 666);
+    g_assert(child);
+    g_assert_cmpint(child->pid, ==, 666);
+    g_assert(child->flags & TCHILD_NEEDSETUP);
+    g_assert(!(child->flags & TCHILD_INSYSCALL));
+    g_assert_cmpint(child->sno, ==, 0xbadca11);
+    g_assert_cmpint(child->retval, ==, -1);
 
-    tchild_free (&children);
+    g_hash_table_destroy(children);
 }
 
-static void
-test2 (void)
+static void test2(void)
 {
-    GSList *children = NULL;
+    GHashTable *children = g_hash_table_new_full(g_direct_hash, g_direct_equal, NULL, tchild_free_one);
 
-    tchild_new (&children, 666);
-    tchild_free (&children);
+    tchild_new(children, 666);
+    tchild_new(children, 667);
+    tchild_new(children, 668);
 
-    g_assert (! children);
+    tchild_delete(children, 666);
+
+    g_assert(NULL == tchild_find(children, 666));
+
+    g_hash_table_destroy(children);
 }
 
-static void
-test3 (void)
-{
-    GSList *children = NULL;
-
-    tchild_new (&children, 666);
-    tchild_delete (&children, 666);
-
-    g_assert (! children);
-}
-
-static void
-test4 (void)
-{
-    GSList *children = NULL;
-    GSList *entry = NULL;
-
-    tchild_new (&children, 666);
-    tchild_new (&children, 667);
-    tchild_new (&children, 668);
-
-    tchild_delete (&children, 666);
-
-    for (entry = children; entry; entry = g_slist_next (entry))
-        g_assert_cmpint (((struct tchild *)entry->data)->pid, !=, 666);
-
-    tchild_free (&children);
-}
-
-static void
-no_log (const gchar *log_domain, GLogLevelFlags log_level, const gchar *message, gpointer user_data)
+static void no_log (const gchar *log_domain, GLogLevelFlags log_level, const gchar *message, gpointer user_data)
 {
 }
 
-int
-main (int argc, char **argv)
+int main(int argc, char **argv)
 {
-    setenv(ENV_NO_CONFIG, "1", 1);
+    g_setenv(ENV_NO_CONFIG, "1", 1);
     sydbox_config_load(NULL);
 
     g_test_init (&argc, &argv, NULL);
@@ -99,10 +70,7 @@ main (int argc, char **argv)
     g_log_set_default_handler (no_log, NULL);
 
     g_test_add_func ("/children/new", test1);
-    g_test_add_func ("/children/free", test2);
-
-    g_test_add_func ("/children/delete-first", test3);
-    g_test_add_func ("/children/delete", test4);
+    g_test_add_func ("/children/delete", test2);
 
     return g_test_run ();
 }
