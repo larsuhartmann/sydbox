bddf7f63ff65e54e374a6d0a32e7734204bf37dc
Ali Polatel <polatel@gmail.com>
2009-08-28 23:35:18 +0300

    Simplify logic, kill dispatch_flags*

--- src/dispatch.c
+++ src/dispatch.c
@@ -40,95 +40,14 @@ static const struct syscall_name {
 
 static GHashTable *flags = NULL;
 
-static int dispatch_flags(int personality G_GNUC_UNUSED, int sno)
-{
-    for (unsigned int i = 0; -1 != syscalls[i].no; i++) {
-        if (syscalls[i].no == sno)
-            return syscalls[i].flags;
-    }
-    return -1;
-}
-
 void dispatch_init(void)
 {
     if (flags != NULL)
         return;
 
     flags = g_hash_table_new(g_direct_hash, g_direct_equal);
-#define REGISTER_SYSCALL(no)                                                                            \
-    do {                                                                                                \
-        g_hash_table_insert(flags, GINT_TO_POINTER((no)), GINT_TO_POINTER(dispatch_flags(-1, (no))));   \
-    } while (0)
-
-    REGISTER_SYSCALL(__NR_chmod);
-    REGISTER_SYSCALL(__NR_chown);
-#if defined(__NR_chown32)
-    REGISTER_SYSCALL(__NR_chown32);
-#endif
-    REGISTER_SYSCALL(__NR_open);
-    REGISTER_SYSCALL(__NR_creat);
-    REGISTER_SYSCALL(__NR_stat);
-#if defined(__NR_stat64)
-    REGISTER_SYSCALL(__NR_stat64);
-#endif
-    REGISTER_SYSCALL(__NR_lchown);
-#if defined(__NR_lchown32)
-    REGISTER_SYSCALL(__NR_lchown32);
-#endif
-    REGISTER_SYSCALL(__NR_link);
-    REGISTER_SYSCALL(__NR_mkdir);
-    REGISTER_SYSCALL(__NR_mknod);
-    REGISTER_SYSCALL(__NR_access);
-    REGISTER_SYSCALL(__NR_rename);
-    REGISTER_SYSCALL(__NR_rmdir);
-    REGISTER_SYSCALL(__NR_symlink);
-    REGISTER_SYSCALL(__NR_truncate);
-#if defined(__NR_truncate64)
-    REGISTER_SYSCALL(__NR_truncate64);
-#endif
-    REGISTER_SYSCALL(__NR_mount);
-#if defined(__NR_umount)
-    REGISTER_SYSCALL(__NR_umount);
-#endif
-#if defined(__NR_umount2)
-    REGISTER_SYSCALL(__NR_umount2);
-#endif
-#if defined(__NR_utime)
-    REGISTER_SYSCALL(__NR_utime);
-#endif
-#if defined(__NR_utimes)
-    REGISTER_SYSCALL(__NR_utimes);
-#endif
-    REGISTER_SYSCALL(__NR_unlink);
-    REGISTER_SYSCALL(__NR_openat);
-    REGISTER_SYSCALL(__NR_mkdirat);
-    REGISTER_SYSCALL(__NR_mknodat);
-    REGISTER_SYSCALL(__NR_fchownat);
-    REGISTER_SYSCALL(__NR_unlinkat);
-    REGISTER_SYSCALL(__NR_renameat);
-    REGISTER_SYSCALL(__NR_linkat);
-    REGISTER_SYSCALL(__NR_symlinkat);
-    REGISTER_SYSCALL(__NR_fchmodat);
-    REGISTER_SYSCALL(__NR_faccessat);
-#if defined(__NR_socketcall)
-    REGISTER_SYSCALL(__NR_socketcall);
-#endif
-#if defined(__NR_socket)
-    REGISTER_SYSCALL(__NR_socket);
-#endif
-#if defined(__NR_connect)
-    REGISTER_SYSCALL(__NR_connect);
-#endif
-#if defined(__NR_bind)
-    REGISTER_SYSCALL(__NR_bind);
-#endif
-    REGISTER_SYSCALL(__NR_execve);
-    REGISTER_SYSCALL(__NR_chdir);
-    REGISTER_SYSCALL(__NR_fchdir);
-#if defined(POWERPC)
-    REGISTER_SYSCALL(__NR_clone);
-#endif
-#undef REGISTER_SYSCALL
+    for (unsigned int i = 0; -1 != syscalls[i].no; i++)
+        g_hash_table_insert(flags, GINT_TO_POINTER(syscalls[i].no), GINT_TO_POINTER(syscalls[i].flags));
 }
 
 void dispatch_free(void)
