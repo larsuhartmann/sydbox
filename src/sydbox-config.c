9d267a7fbf67635c065c2d30c7eeccac2b19af54
Ali Polatel <polatel@gmail.com>
2009-08-31 20:23:35 +0300

    Move SYDBOX_NET_WHITELIST parsing
    Move SYDBOX_NET_WHITELIST parsing to
    sydbox_config_update_from_environment() so it'll work even if
    SYDBOX_NO_CONFIG is set.

--- src/sydbox-config.c
+++ src/sydbox-config.c
@@ -415,26 +415,11 @@ bool sydbox_config_load(const gchar * const file)
     }
 
     // Get net.whitelist
-    char **netwhitelist;
-    netwhitelist = g_key_file_get_string_list(config_fd, "net", "whitelist", NULL, NULL);
+    char **netwhitelist = g_key_file_get_string_list(config_fd, "net", "whitelist", NULL, NULL);
     if (NULL != netwhitelist) {
         for (unsigned int i = 0; NULL != netwhitelist[i]; i++) {
             if (0 > netlist_new_from_string(&config->network_whitelist, netwhitelist[i], false)) {
                 g_printerr("error: malformed address `%s' at position %d of net.whitelist\n", netwhitelist[i], i);
-                g_strfreev(netwhitelist);
-                g_key_file_free(config_fd);
-                g_free(config);
-                return false;
-            }
-        }
-        g_strfreev(netwhitelist);
-    }
-
-    if (g_getenv(ENV_NET_WHITELIST)) {
-        netwhitelist = g_strsplit(g_getenv(ENV_NET_WHITELIST), ";", 0);
-        for (unsigned int i = 0; NULL != netwhitelist[i]; i++) {
-            if (0 > netlist_new_from_string(&config->network_whitelist, netwhitelist[i], false)) {
-                g_printerr("error: malformed address `%s' at position %d of "ENV_NET_WHITELIST"\n", netwhitelist[i], i);
                 g_strfreev(netwhitelist);
                 g_key_file_free(config_fd);
                 g_free(config);
@@ -456,6 +441,22 @@ void sydbox_config_update_from_environment(void)
 
     g_info("extending path list using environment variable " ENV_EXEC_ALLOW);
     pathlist_init(&config->exec_prefixes, g_getenv(ENV_EXEC_ALLOW));
+
+    g_info("extending network whitelist using environment variable " ENV_NET_WHITELIST);
+    if (g_getenv(ENV_NET_WHITELIST)) {
+        char **netwhitelist = g_strsplit(g_getenv(ENV_NET_WHITELIST), ";", 0);
+        for (unsigned int i = 0; NULL != netwhitelist[i]; i++) {
+            if (0 > netlist_new_from_string(&config->network_whitelist, netwhitelist[i], true)) {
+                g_critical("error: malformed address `%s' at position %d of "ENV_NET_WHITELIST"\n",
+                        netwhitelist[i], i);
+                g_printerr("error: malformed address `%s' at position %d of "ENV_NET_WHITELIST"\n",
+                        netwhitelist[i], i);
+                g_strfreev(netwhitelist);
+                exit(-1);
+            }
+        }
+        g_strfreev(netwhitelist);
+    }
 }
 
 
